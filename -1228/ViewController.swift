f9ab8e5ed16d50d18b6672efd6ab092940f9524c
paulfang <paulfang0723@gmail.com>
2020-12-28 19:14:58 +0800

    Initial Commit

---
+++ -1228/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  -1228
+//
+//  Created by PaulFang on 2020/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
